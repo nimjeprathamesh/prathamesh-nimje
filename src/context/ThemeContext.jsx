fa2288bd79d56371aa4e5ada43d5bdcb3b630ab4
prathamesh-radical <nimjeprathamesh1@gmail.com>
2025-05-07 23:41:41 +0530

    theme changed

--- src/context/ThemeContext.jsx
+++ src/context/ThemeContext.jsx
@@ -5,7 +5,7 @@ import { createContext, useEffect, useState } from 'react';
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-    const [theme, setTheme] = useState('light');
+    const [theme, setTheme] = useState('dark');
 
     useEffect(() => {
         const savedTheme = localStorage.getItem('theme');
